226aa5773c0880e2671fb46d60b736c317545fa7
sae.okamoto <s-okamoto@tact-info.co.jp>
2022-07-15 09:49:02 +0000

    kadai task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: 'tasks#index'
+
+  resources :tasks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'tasks', to: 'tasks#index'
+  #get 'tasks/:id', to: 'tasks#show'
+  #get 'tasks/new', to: 'tasks#new'
+  #post 'tasks', to: 'tasks#create'
+  #get 'tasks/:id/edit', to: 'tasks#edit'
+  #put 'tasks/:id', to: 'tasks#update'
+  #delete 'tasks/:id', to: 'tasks#destory'
+
 end
